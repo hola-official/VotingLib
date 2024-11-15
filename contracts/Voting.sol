cd1c309158e3d7c3e01b1c99e1dee2871d2ba9ed
hola-official <sam@overthetopseo.com>
2024-11-15 11:26:57 +0100

    vote contract implemented

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -3,5 +3,68 @@ pragma solidity ^0.8.27;
 import "./VotingLib.sol";
 
 contract Voting {
-  using VotingLib for VotingLib.Voter;
+    using VotingLib for VotingLib.VotingData;
+
+    VotingLib.VotingData private votingData;
+    address public owner;
+    bool public votingOpen;
+
+    event VoteCast(address indexed voter, uint256 candidateId);
+    event CandidateAdded(uint256 candidateId);
+    event VotingStatusChanged(bool isOpen);
+
+    modifier onlyOwner() {
+        require(msg.sender != address(0), "User not allowed");
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    modifier votingIsOpen() {
+        require(msg.sender != address(0), "User not allowed");
+        require(votingOpen, "Voting is not open");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+        votingOpen = false;
+    }
+
+    function startVoting() external onlyOwner {
+        votingOpen = true;
+        emit VotingStatusChanged(true);
+    }
+
+    function endVoting() external onlyOwner {
+        votingOpen = false;
+        emit VotingStatusChanged(false);
+    }
+
+    function addCandidate(uint256 _candidateId) external onlyOwner {
+        votingData.addCandidate(_candidateId);
+        emit CandidateAdded(_candidateId);
+    }
+
+    function vote(uint256 _candidateId) external votingIsOpen {
+        votingData.castVote(_candidateId, msg.sender);
+        emit VoteCast(msg.sender, _candidateId);
+    }
+
+    function getVotesForCandidate(
+        uint256 _candidateId
+    ) external view returns (uint256) {
+        return votingData.getVotes(_candidateId);
+    }
+
+    function getWinner()
+        external
+        view
+        returns (uint256 winningCandidateId, uint256 winningVoteCount)
+    {
+        return votingData.calculateWinner();
+    }
+
+    function hasVoted(address _voter) external view returns (bool) {
+        return votingData.hasVoted[_voter];
+    }
 }
--- contracts/VotingLib.sol
+++ contracts/VotingLib.sol
@@ -1,33 +1,82 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.27;
+pragma solidity ^0.8.0;
 
 library VotingLib {
-    struct Voter {
-        uint256 aadharNumber;
-        string name;
-        uint8 age;
-        uint8 stateCode;
-        uint8 constituencyCode;
-        bool isAlive;
-        uint256 votedTo;
+    struct VotingData {
+        mapping(address => bool) hasVoted;
+        mapping(uint256 => uint256) votesReceived;
+        uint256[] candidateIds;
+        uint256 totalVotes;
     }
 
-    struct Candidate {
-        string name;
-        string partyShortcut;
-        string partyFlag;
-        uint256 nominationNumber;
-        uint8 stateCode;
-        uint8 constituencyCode;
+    function addCandidate(
+        VotingData storage self,
+        uint256 _candidateId
+    ) public {
+        require(msg.sender != address(0), "User not allowed");
+        require(
+            !isCandidateExists(self, _candidateId),
+            "Candidate already exists"
+        );
+        self.candidateIds.push(_candidateId);
+        self.votesReceived[_candidateId] = 0;
     }
 
-    struct Results {
-        string name;
-        string partyShortcut;
-        string partyFlag;
-        uint256 voteCount;
-        uint256 nominationNumber;
-        uint8 stateCode;
-        uint8 constituencyCode;
+    function isCandidateExists(
+        VotingData storage self,
+        uint256 _candidateId
+    ) public view returns (bool) {
+        require(msg.sender != address(0), "User not allowed");
+        for (uint i = 0; i < self.candidateIds.length; i++) {
+            if (self.candidateIds[i] == _candidateId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function castVote(
+        VotingData storage self,
+        uint256 _candidateId,
+        address _voter
+    ) public {
+        require(msg.sender != address(0), "User not allowed");
+        require(!self.hasVoted[_voter], "Voter has already voted");
+        require(isCandidateExists(self, _candidateId), "Invalid candidate");
+
+        self.hasVoted[_voter] = true;
+        self.votesReceived[_candidateId]++;
+        self.totalVotes++;
+    }
+
+    function getVotes(
+        VotingData storage self,
+        uint256 _candidateId
+    ) public view returns (uint256) {
+        require(msg.sender != address(0), "User not allowed");
+        require(isCandidateExists(self, _candidateId), "Invalid candidate");
+        return self.votesReceived[_candidateId];
+    }
+
+    function calculateWinner(
+        VotingData storage self
+    ) public view returns (uint256, uint256) {
+        require(msg.sender != address(0), "User not allowed");
+        require(self.totalVotes > 0, "No votes cast yet");
+
+        uint256 winningVoteCount = 0;
+        uint256 winningCandidateId = 0;
+
+        for (uint i = 0; i < self.candidateIds.length; i++) {
+            uint256 candidateId = self.candidateIds[i];
+            uint256 voteCount = self.votesReceived[candidateId];
+
+            if (voteCount > winningVoteCount) {
+                winningVoteCount = voteCount;
+                winningCandidateId = candidateId;
+            }
+        }
+
+        return (winningCandidateId, winningVoteCount);
     }
 }
